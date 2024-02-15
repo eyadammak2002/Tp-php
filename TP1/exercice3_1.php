53b192d1d957c4184bb1900f37334bc9ac259159
eyadammak2002 <eyadammak.ig@gmail.com>
2024-02-15 09:14:23 +0100

    tableau associative exercice 3  2éme

--- TP1/exercice3_1.php
+++ TP1/exercice3_1.php
@@ -1,44 +1,22 @@
-<html>
-<body>
-<?php
+<!DOCTYPE html> 
+<html>     
+    <head>
+        <title>Exeercice 3</title>
+    </head>     
+    <body>     
+        <fieldset>         
+            <h1> Les tabeaux </h1>        
+             <?php                 
+             $Prenom= array("ali", "salma", "mohamed" , "ines" );   
+             $Nom= array("benAli", "benSalma", "benMohamed" , "benInes" );
+            $Age= array(20,23 ,19 ,22   );         
+            $Ville= array("sfax", "sousse", "gabes" , "sfax" );         
+             
+            echo "<table border='1'> <tr> <th> Prenom </th><th>Nom</th><th>Age</th><th>Ville</th></tr> ";
+            for($i=0;$i<count($Prenom);$i++){
+                echo"<tr><td>". $Prenom[$i] . " </td> <td>".$Nom[$i]. "</td> <td>". $Age[$i]."</td> <td>". $Ville[$i]."</td></tr>";
+            }
+            echo "</table>";
 
-$tprenom=array("ali","salma","mohamed","iness");
-$tnom=array("benAli","benSalma","benMohamed","benIness");
-$tage=array(20,23,19,22);
-$tville=array("sfax","sousse","gabes","sfax");
 
-?>
-
-<p style="border: 1px solid black; padding: 10px; width: 300px; margin: 0 auto;">
-      LES TABLEAUX
-    
-
-
-<table border="1">
-    <tr>
-         <th>Code</th>
-        <th>prenom</th>
-        <th>nom</th>
-        <th>age</th>
-        <th>ville</th>
-    </tr>
-    <?php
-    // Boucle pour parcourir toutes les entrées
-    for ($i = 0; $i < sizeof($tprenom); $i++) {
-        echo "<tr>";
-        // Affichage des prénoms
-        echo "<td>" . $tprenom[$i] . "</td>";
-        // Affichage des noms
-        echo "<td>" . $tnom[$i] . "</td>";
-        // Affichage des âges
-        echo "<td>" . $tage[$i] . "</td>";
-        // Affichage des villes
-        echo "<td>" . $tville[$i] . "</td>";
-        echo "</tr>";
-    }
-    ?>
-</table>  
-</p> 
-</body>
-</html>
-
+ 
